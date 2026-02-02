9a8fc7410d15669123da61a04c27221725f2790d
mrwasif-dev <happyprincetemp@gmail.com>
2026-02-02 18:08:01 +0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,23 +1,18 @@
 const fs = require('fs');
-const path = require('path');
-
 const plugins = {};
-const pluginDir = './plugins';
 
-if (fs.existsSync(pluginDir)) {
-    const files = fs.readdirSync(pluginDir)
-        .filter(file => file.endsWith('.js'));
+if (fs.existsSync('./plugins')) {
+    const files = fs.readdirSync('./plugins').filter(f => f.endsWith('.js'));
     
     files.forEach(file => {
+        const name = file.replace('.js', '');
         try {
-            const pluginName = path.basename(file, '.js');
-            plugins[pluginName] = require(`./plugins/${file}`);
-            console.log(`✅ Plugin: ${pluginName}`);
-        } catch (err) {
-            console.log(`❌ ${file}: ${err.message}`);
+            plugins[name] = require(`./plugins/${file}`);
+            console.log(`✅ Plugin loaded: ${name}`);
+        } catch (e) {
+            console.log(`❌ Failed to load ${file}:`, e.message);
         }
     });
 }
 
-console.log(`📦 Total: ${Object.keys(plugins).length} plugins`);
 module.exports = plugins;
