a3e609ebe3de64007d1a5edee68387043311aac0
mrwasif-dev <happyprincetemp@gmail.com>
2026-02-02 17:59:33 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -4,14 +4,12 @@ module.exports = {
         const start = Date.now();
         const jid = message.key.remoteJid;
         
-        // پہلا میسج: 🏓 PONG!
         const sent = await sock.sendMessage(jid, { 
             text: "🏓 PONG!" 
         });
         
         const ping = Date.now() - start;
         
-        // ایڈٹ کر کے صرف Response Time رکھیں
         await sock.sendMessage(jid, {
             text: `⚡ Response Time: ${ping}ms`,
             edit: { remoteJid: sent.key.remoteJid, id: sent.key.id, fromMe: true }
